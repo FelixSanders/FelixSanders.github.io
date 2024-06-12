24bfe7f6e9798ffb09bd0003e5342f24b54ecf0b
FSKConnect <felixsanderskusuma@gmail.com>
2024-06-12 23:53:08 +0700

    setup

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function () {
+    var header = document.querySelector(".header");
+    var headerlogo = document.querySelector(".header-logo");
+
+    var shouldToggle = window.scrollY > 20;
+    header.classList.toggle("visible", shouldToggle);
+    headerlogo.classList.toggle("white", shouldToggle);
+});
+
